eb494926d5202d8a65d1d8d4f2230bdd7d5d586d
wlp2s0 <me@mattiacampagna.com>
2025-02-24 16:58:19 +0100

    Refactor getThresholds function to separate merge threshold into min and max ratios

--- src/functions/getThresholds.js
+++ src/functions/getThresholds.js
@@ -1,6 +1,7 @@
 export const getThresholds = ({
     dimension,
-    mergeThresholdRatio,
+    minMergeThresholdRatio,
+    maxMergeThresholdRatio,
     marginThresholdRatio,
     minWidthThresholdRatio,
     minHeightThresholdRatio,
@@ -8,10 +9,11 @@ export const getThresholds = ({
     maxHeightThresholdRatio
 }) => {
     const marginThreshold = Math.floor(Math.max(dimension * marginThresholdRatio, 1));
-    const mergeThreshold = Math.floor(Math.max(dimension * mergeThresholdRatio, 1));
+    const minMergeThreshold = Math.floor(Math.max(dimension * minMergeThresholdRatio, 1));
+    const maxMergeThreshold = Math.floor(Math.max(dimension * maxMergeThresholdRatio, 1));
     const minWidth = Math.floor(Math.max(dimension * minWidthThresholdRatio, 1));
     const minHeight = Math.floor(Math.max(dimension * minHeightThresholdRatio, 1));
     const maxWidth = Math.floor(Math.max(dimension * maxWidthThresholdRatio, 1));
     const maxHeight = Math.floor(Math.max(dimension * maxHeightThresholdRatio, 1));
-    return { marginThreshold, mergeThreshold, minWidth, minHeight, maxWidth, maxHeight };
+    return { marginThreshold, minMergeThreshold, maxMergeThreshold, minWidth, minHeight, maxWidth, maxHeight };
 }
