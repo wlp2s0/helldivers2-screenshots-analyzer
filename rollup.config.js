4ac244db63103e21a96036ce7e55c3bbcbefece7
wlp2s0 <me@mattiacampagna.com>
2025-03-03 10:34:28 +0100

    Update package.json and rollup.config.js for module type and plugin adjustment

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
-import { externals } from 'rollup-plugin-node-externals';
+import { nodeExternals } from 'rollup-plugin-node-externals'
 
 export default {
     input: 'src/parseImage.js',
@@ -11,7 +11,7 @@ export default {
         format: 'cjs',
     },
     plugins: [
-        externals(),
+        nodeExternals(),
         nodeResolve(),
         commonjs(),
         babel({
