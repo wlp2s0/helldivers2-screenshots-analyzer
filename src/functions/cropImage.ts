97170aec1aefe595b99eb72f26c372b632744906
wlp2s0 <me@mattiacampagna.com>
2025-03-03 17:26:28 +0100

    Enhance cropImage function to include yCropOffsetRatio parameter for improved cropping control

--- src/functions/cropImage.ts
+++ src/functions/cropImage.ts
@@ -4,18 +4,18 @@ import type { JimpImage } from "../types/JimpImage.js";
  * Crops an image to the specified ratios and saves it to the target path.
  *
  * @param image - The image object to be cropped.
- * @param targetPath - The path where the cropped image will be saved.
- * @param [cropRatioWidth=0.55] - The width ratio to crop the image. Default is 0.55.
- * @param [cropRatioHeight=0.7] - The height ratio to crop the image. Default is 0.7.
+ * @param cropRatioWidth - The width ratio to crop the image. Default is 0.55.
+ * @param cropRatioHeight - The height ratio to crop the image. Default is 0.7.
+ * @param yCropOffsetRatio - The ratio to offset the crop on the Y-axis. Default is 0.1.
  * @returns - A promise that resolves to an object containing the cropped image.
  */
-export async function cropImage(image: JimpImage, cropRatioWidth: number = 0.55, cropRatioHeight: number = 0.7): Promise<JimpImage> {
+export async function cropImage(image: JimpImage, cropRatioWidth: number = 0.55, cropRatioHeight: number = 0.7, yCropOffsetRatio: number = 0.1): Promise<JimpImage> {
     const { width, height } = image.bitmap;
 
     const cropWidth = Math.floor(width * cropRatioWidth);
     const cropHeight = Math.floor(height * cropRatioHeight);
     const cropX = Math.floor((width - cropWidth) / 2);
-    const cropY = Math.floor((height - cropHeight) / 2);
+    const cropY = Math.floor((height - cropHeight) / 2 + height * yCropOffsetRatio);
 
-    return image.crop({ x: cropX, y: cropY, w: cropWidth, h: cropHeight }) as unknown as JimpImage;
+    return image.clone().crop({ x: cropX, y: cropY, w: cropWidth, h: cropHeight }) as unknown as JimpImage;
 }
