c6e2218ec9159ba8ed95810f47c32b33a42da0fa
wlp2s0 <me@mattiacampagna.com>
2025-03-04 15:47:49 +0100

    Refactor rollup configuration for improved TypeScript support and formatting consistency

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,40 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { babel } from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser';
-import { nodeExternals } from 'rollup-plugin-node-externals';
-import typescript from '@rollup/plugin-typescript';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { babel } from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+import { nodeExternals } from "rollup-plugin-node-externals";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-    input: 'src/index.ts',
-    output: [{
-        file: 'dist/index.cjs',
-        format: 'cjs',
-        sourcemap: true
-    }, {
-        file: 'dist/index.mjs',
-        format: 'es',
-        sourcemap: true
-    }],
-    plugins: [
-        nodeExternals(),
-        nodeResolve(),
-        commonjs(),
-        typescript(),
-        babel({
-            babelHelpers: 'bundled',
-            presets: ['@babel/preset-env']
-        }),
-        terser()
-    ]
+	input: "src/index.ts",
+	output: [
+		{
+			file: "dist/index.cjs",
+			format: "cjs",
+			sourcemap: true,
+		},
+		{
+			file: "dist/index.mjs",
+			format: "es",
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		nodeExternals(),
+		nodeResolve(),
+		commonjs(),
+		typescript({
+			tsconfig: "./tsconfig.json",
+			compilerOptions: {
+				rootDir: "./src",
+				removeComments: false,
+				declaration: true,
+			},
+		}),
+		babel({
+			babelHelpers: "bundled",
+			presets: ["@babel/preset-env"],
+		}),
+		terser(),
+	],
 };
